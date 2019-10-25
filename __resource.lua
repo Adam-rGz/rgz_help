d94000711df53ee9cc8f7dff7952ddb3b4f938e2
Christopher M <10535902+cm8263@users.noreply.github.com>
2019-10-25 22:22:17 +1000

    Removed unneeded ESX components

--- __resource.lua
+++ __resource.lua
@@ -1,21 +1,13 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-server_scripts {
-	'@mysql-async/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'server.lua'
-}
+server_script 'server.lua'
 
-client_scripts {
-	'@es_extended/locale.lua',
-	'client/client.lua'
-}
+client_script 'client/client.lua'
 
-ui_page('client/html/UI.html')
+ui_page 'client/html/UI.html'
 
-files({
+files{
     'client/html/UI.html',
     'client/html/img/menu.png',
-    'client/html/img/menu.PNG',
     'client/html/style.css'
-})
+}
